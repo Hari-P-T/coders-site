ba13053d72b724b9a3a14be35ac5f73bfa06b2a4
Hari-P-T <112808592+Hari-P-T@users.noreply.github.com>
2024-05-14 10:23:07 +0530

    Update App.js

--- Online Compiler/my-compiler/src/App.js
+++ Online Compiler/my-compiler/src/App.js
@@ -1,29 +1,57 @@
-import React, {useState, useEffect} from 'react';
+// import React, {useState, useEffect} from 'react';
+// import Home from './Home';
+// import Signup from './Signup';
+// import Login from './Login';
+// import Problem1 from './Problem1';
+// import Leaderboard from './LeaderBoard';
+// import { BrowserRouter as Router} from 'react-router-dom';
+// import {Routes, Route} from 'react-router-dom';
+ 
+// function App() {
+ 
+//   return (
+//     <Router>
+//       <div>
+//         <section>                              
+//             <Routes>
+//                <Route path="/" element={<Home/>}/>
+//                <Route path="/signup" element={<Signup/>}/>
+//                <Route path="/login" element={<Login/>}/>
+//                <Route path="/problem1" element={<Problem1/>}/>
+//                <Route path="/leaderboard" element={<Leaderboard/>}/>
+//             </Routes>                    
+//         </section>
+//       </div>
+//     </Router>
+//   );
+// }
+ 
+// export default App;
+
+import React from 'react';
 import Home from './Home';
 import Signup from './Signup';
 import Login from './Login';
 import Problem1 from './Problem1';
 import Leaderboard from './LeaderBoard';
-import { BrowserRouter as Router} from 'react-router-dom';
-import {Routes, Route} from 'react-router-dom';
- 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
- 
-  return (
-    <Router>
-      <div>
-        <section>                              
-            <Routes>
-               <Route path="/" element={<Home/>}/>
-               <Route path="/signup" element={<Signup/>}/>
-               <Route path="/login" element={<Login/>}/>
-               <Route path="/problem1" element={<Problem1/>}/>
-               <Route path="/leaderboard" element={<Leaderboard/>}/>
-            </Routes>                    
-        </section>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div>
+                <section>
+                    <Routes>
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/problem1" element={<Problem1 />} />
+                        <Route path="/leaderboard" element={<Leaderboard />} />
+                    </Routes>
+                </section>
+            </div>
+        </Router>
+    );
 }
- 
+
 export default App;
